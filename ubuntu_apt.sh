b6db9ef1270cbfc57a063377a3eeb3e2232f75de
Chua Hou <human@chuahou.dev>
2019-07-24 11:22:27 +0800

    Add ubuntu apt install script (currently mostly empty, to be filled in when next installing ubuntu)

---
+++ ubuntu_apt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Uses `apt` to install the common packages I will need on every new Ubuntu
+# installation. I started working on this late, so it will be incomplete.
+#
+# Run as root.
+
+# update package lists
+apt update
+
+# install tools
+apt install git tmux gitg
+
+# TODO: Will add more things when I realise I need them
+

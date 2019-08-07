dc245ef7879449423dac696638e721e5ed0cff9c
Chua Hou <human@chuahou.dev>
2019-08-07 15:18:54 +0800

    Add git setup script

---
+++ git_config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Configures git settings
+
+git config --global user.email "human@chuahou.dev"
+git config --global user.name "Chua Hou"
+git config --global credential.helper store
+git config --global credential.helper cache 900

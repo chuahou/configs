fdf123ae33f5a096ccfe9b7381df51a98bbd4f5f
Chua Hou <human@chuahou.dev>
2019-08-07 15:21:21 +0800

    Move profiles to folder and add script to automatically copy them

---
+++ install_profiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Copies profile, bash_aliases etc to ~
+#
+# DO NOT RUN AS ROOT
+
+cp profiles/bash_aliases ~/.bash_aliases
+cp profiles/profile ~/.profile
+cp profiles/vimrc ~/.vimrc

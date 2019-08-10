98fb894a1c1458149bad61f86b46becaffbe8248
Chua Hou <human@chuahou.dev>
2019-08-10 14:45:01 +0800

    Add VS code extensions list

---
+++ vsextensions.sh
@@ -0,0 +1,7 @@
+code --install-extension attilabuti.brainfuck-syntax
+code --install-extension bungcip.better-toml
+code --install-extension cschlosser.doxdocgen
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension rust-lang.rust
+code --install-extension vscodevim.vim

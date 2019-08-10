bbcc0590db9a3932f06c5bad4764a38a3eca7461
Chua Hou <human@chuahou.dev>
2019-08-10 15:28:45 +0800

    Add automated VS code extension management script

--- vsextensions.sh
+++ vsextensions.sh
@@ -1,8 +1,49 @@
 #!/bin/bash
-code --install-extension attilabuti.brainfuck-syntax
-code --install-extension bungcip.better-toml
-code --install-extension cschlosser.doxdocgen
-code --install-extension ms-python.python
-code --install-extension ms-vscode.cpptools
-code --install-extension rust-lang.rust
-code --install-extension vscodevim.vim
+#
+# VS extension management script.
+
+# prints usage
+function print_usage
+{
+    echo "USAGE:"
+    echo "    ./vsextensions.sh [OPERATION] [EXTENSIONS FILE]"
+    echo
+    echo "Where [OPERATION] is one of:"
+    echo "    - exportnew   Export current extensions and overwrite extensions list"
+    echo "    - export      Export current extensions and append new ones to extensions list"
+    echo "    - install     Install extensions from current extensions list"
+    echo
+    echo "and [EXTENSIONS FILE] is the filename of the extensions list."
+    echo
+    echo "Example:"
+    echo "    ./vsextensions.sh export extensions.txt"
+    echo "    ./vsextensions.sh install extensions.txt"
+}
+
+# read command line args
+
+# print usage if less than or more than 2 args
+if [ "$#" -ne "2" ]
+then
+    print_usage
+fi
+
+# exportnew
+if [ "$1" = "exportnew" ]; then
+    code --list-extensions > temp
+    sort temp > $2
+
+# export
+elif [ "$1" = "export" ]; then
+    code --list-extensions > temp
+    sort $2 > current
+    comm -23 temp current >> current
+    sort current > $2
+
+# install
+elif [ "$1" = "install" ]; then
+    cat $2 | xargs -L 1 code --install-extension
+fi
+
+# clean temp files
+rm -f temp current || true

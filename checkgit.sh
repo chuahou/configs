af79d5e7b6b43e5f46aac94f14e2e41d9350646f
Chua Hou <human@chuahou.dev>
2019-08-12 13:27:19 +0800

    Update checkgit script to automatically check if the repo needs action

--- checkgit.sh
+++ checkgit.sh
@@ -2,4 +2,33 @@
 #
 # Checks all folders in the same level and prints git status.
 
-find .. -mindepth 1 -maxdepth 1 -type d \( ! -name . \) -exec bash -c 'echo "{}" && cd "{}" && git status && echo && echo' \;
+for dir in `find ${PWD}/.. -mindepth 1 -maxdepth 1 -type d`; do
+    cd "$dir"
+    printstatus=0
+    gitstatus=$(git status)
+
+    # check for unstaged
+    echo $gitstatus | grep "nothing to commit" > /dev/null
+    if [ $? -ne 0 ]; then
+        printstatus=$((printstatus + 1))
+    fi
+
+    # check for staged
+    echo $gitstatus | grep "to be committed" > /dev/null
+    if [ $? -eq 0 ]; then
+        printstatus=$((printstatus + 1))
+    fi
+
+    # check for committed but ahead
+    echo $gitstatus | grep "ahead of" > /dev/null
+    if [ $? -eq 0 ]; then
+        printstatus=$((printstatus + 1))
+    fi
+
+    # if should print, print status
+    if [ "$printstatus" -gt "0" ]; then
+        pwd
+        git status
+        echo
+    fi
+done

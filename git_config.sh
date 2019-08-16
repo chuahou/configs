69e7e784542cdfc66f44450c13f7bf8480e7fe3f
Chua Hou <human@chuahou.dev>
2019-08-16 21:38:29 +0800

    Make git config cache instead of store

--- git_config.sh
+++ git_config.sh
@@ -11,6 +11,5 @@ fi
 
 git config --global user.email "human@chuahou.dev"
 git config --global user.name "Chua Hou"
-git config --global credential.helper store
 git config --global credential.helper cache 900
 git config --global core.editor "vim"

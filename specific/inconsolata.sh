7c267942f9714f03b4b8ff812bc7be0c4196be69
Chua Hou <human@chuahou.dev>
2019-08-27 15:43:19 +0800

    Add Inconsolata font

---
+++ specific/inconsolata.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Downloads the inconsolata font from the Google Fonts repo on GitHub.
+#
+# Do NOT run as root.
+
+# check for root
+if [ "$EUID" -eq 0 ]
+then
+    echo "Do not run as ROOT."
+    exit
+fi
+
+# get files
+wget https://github.com/google/fonts/raw/master/ofl/inconsolata/Inconsolata-Bold.ttf
+wget https://github.com/google/fonts/raw/master/ofl/inconsolata/Inconsolata-Regular.ttf

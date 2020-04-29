481c93167d396ddaef4075dde8ba0444acffbea6
Chua Hou <human+github@chuahou.dev>
2020-04-29 12:13:13 +0800

    Make regolith part of profiles instead of specific

--- install_profiles.sh
+++ install_profiles.sh
@@ -11,13 +11,22 @@ then
     exit
 fi
 
+# copy home dotfiles
 cp profiles/bash_aliases ~/.bash_aliases
 cp profiles/profile ~/.profile
 cp profiles/vimrc ~/.vimrc
 cp profiles/bashrc ~/.bashrc
 cp profiles/ghci ~/.ghci
+cp profiles/Xresources-regolith ~/.Xresources-regolith
+
+# make directories for non-~ configs
+mkdir -p ~/.config/gtk-3.0 ~/.config/dunst ~/.config/regolith
+
+# copy non-~ configs
 cp profiles/gtk.css ~/.config/gtk-3.0/gtk.css
 cp profiles/dunstrc ~/.config/dunst/dunstrc
+cp profiles/regolith/* -r ~/.config/regolith
 
+# make directories for vim
 mkdir -p ~/.vim
 mkdir -p ~/.vim/backup

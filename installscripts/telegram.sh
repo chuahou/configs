ef9d0e7469962b8a7bb88197e407ce63ab71ddf7
Chua Hou <human+github@chuahou.dev>
2020-04-30 20:24:07 +0700

    Add check for already installed Telegram for telegram.sh

--- installscripts/telegram.sh
+++ installscripts/telegram.sh
@@ -7,6 +7,9 @@
 # check not root
 ../scripts/check_root.sh user || exit 1
 
+# check if already exists
+test -d ~/Telegram && exit 1
+
 # download archive
 wget https://telegram.org/dl/desktop/linux -O t.tar.xz
 tar xJf t.tar.xz

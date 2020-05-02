cf87708b5606deb99c0bb3609420f92a74a7464b
Chua Hou <human+github@chuahou.dev>
2020-05-02 18:43:10 +0800

    Make dynamic_wallpaper.sh check out install branch

--- scripts/dynamic_wallpaper.sh
+++ scripts/dynamic_wallpaper.sh
@@ -7,5 +7,9 @@
 # check not root
 ./check_root.sh user || exit 1
 
-mkdir -p ~/Projects
-git clone https://chuahou@github.com/chuahou/dynamic-wallpaper.git ~/Projects/dynamic-wallpaper
+TARGET=~/Projects/dynamic-wallpaper
+mkdir -p $(dirname $TARGET)
+git clone https://chuahou@github.com/chuahou/dynamic-wallpaper.git \
+	~/Projects/dynamic-wallpaper
+cd $TARGET
+git checkout install

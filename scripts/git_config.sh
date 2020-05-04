8e93f6b6bd50426f1313dfbe6cd64a83bedb2385
Chua Hou <human+github@chuahou.dev>
2020-05-04 12:45:50 +0800

    Update git config to use nvim if possible and vim otherwise

--- scripts/git_config.sh
+++ scripts/git_config.sh
@@ -10,4 +10,8 @@
 git config --global user.email "human+github@chuahou.dev"
 git config --global user.name "Chua Hou"
 git config --global credential.helper cache 900
-git config --global core.editor "vim"
+if $(command -v nvim > /dev/null); then
+	git config --global core.editor nvim # use nvim if present
+else
+	git config --global core.editor vim # use nvim if present
+fi

67d28057db2a3d5d4b059ab79f3032aafee63acb
Chua Hou <human+github@chuahou.dev>
2020-05-13 17:53:12 +0800

    scripts: add sbt install

---
+++ installscripts/sbt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Installs scala-sbt.
+#
+# Run as ROOT.
+
+# check for root
+../scripts/check_root.sh root || exit 1
+
+# install sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
+apt-get update
+apt-get install sbt

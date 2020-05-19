e16d94cfe82ad2e2a84aac3505cce4cb3c749d68
Chua Hou <human+github@chuahou.dev>
2020-05-19 17:40:20 +0800

    zsh: add powerlevel10k to ohmyzsh.sh

--- installscripts/ohmyzsh.sh
+++ installscripts/ohmyzsh.sh
@@ -7,5 +7,14 @@
 # check not root
 ../scripts/check_root.sh user || exit 1
 
+# remind to Ctrl+D after running below
+echo "PRESS CTRL+D AFTER INSTALLATION TO RUN REST OF SCRIPT"
+
 # install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# undo the removal of zshrc
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+
+# install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k

e27c6ae61ace54a876a659cf76420c2bb0686d12
Chua Hou <human+github@chuahou.dev>
2020-07-09 20:27:29 +0800

    fuckit.sh: use apt-get instead of apt

--- installscripts/fuckit.sh
+++ installscripts/fuckit.sh
@@ -8,7 +8,7 @@
 ../scripts/check_root.sh root || exit 1
 
 # install Python
-apt install python3-dev python3-pip python3-setuptools -y
+apt-get install python3-dev python3-pip python3-setuptools -y
 
 # install thefuck
 pip3 install thefuck

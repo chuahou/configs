5406ba585dc6b038112140c2a39e4f7a2fe5717d
Chua Hou <human+github@chuahou.dev>
2020-07-09 22:28:31 +0800

    iosevka.sh: create .fonts folder

--- scripts/iosevka.sh
+++ scripts/iosevka.sh
@@ -9,6 +9,7 @@ $(dirname "$0")/check_root.sh user || exit 1
 
 wget https://github.com/be5invis/Iosevka/releases/download/v3.0.0/ttf-iosevka-fixed-3.0.0.zip
 unzip ttf-iosevka-fixed-3.0.0.zip -d tmp
+mkdir -p ~/.fonts
 cp tmp/ttf/*.ttf ~/.fonts
 rm -rf tmp
 rm ttf-iosevka-fixed-3.0.0.zip
